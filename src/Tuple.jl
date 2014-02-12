d10f350a3bce7ad725164fc7d734b9fad8a15018
Will Wilson <will.wilson@foundationdb.com>
2014-02-12 15:32:43 -0500

    Encode numeric types

--- src/Tuple.jl
+++ src/Tuple.jl
@@ -23,8 +23,17 @@ function _find_terminator(v, position::Int)
 end
 
 function _bisect_left(v, item)
-
+    count = 0
+    for i in v
+        if i >= item
+            return count
+        else
+            count += 1
+        end
+    end
 end
+
+const _size_limits = [<<(BigInt(1),(i*8))-1 for i in 0:8]
 
 function _decode(v, position::Int)
 	
@@ -47,5 +56,17 @@ function _encode(v::UTF8String)
 end
 
 function _encode(v::Int)
-	
+    if v == 0
+        return 0x14
+    elseif v > 0
+        n = _bisect_left(_size_limits, v)
+        first = convert(Uint8, char(20+n))
+        rest = hex2bytes(hex(v,16))[9-n:end]
+        return [first, rest]
+    else
+        n = _bisect_left(_size_limits, -v)
+        first = convert(Uint8, char(20-n))
+        rest = hex2bytes(hex(uint64(v+_size_limits[n+1]),16))[9-n:end]
+        return [first, rest]
+    end
 end
